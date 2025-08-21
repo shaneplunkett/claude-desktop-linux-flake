59ea1b7615e04fd7e3d4307828a3fa7a3b815581
Julien Debon <julien.debon@routine.co>
2025-08-21 12:02:36 +0200

    Upgrade to 0.12.112

--- pkgs/claude-desktop.nix
+++ pkgs/claude-desktop.nix
@@ -13,11 +13,11 @@
   perl
 }: let
   pname = "claude-desktop";
-  version = "0.12.55";
+  version = "0.12.112";
   srcExe = fetchurl {
     # NOTE: `?v=0.10.0` doesn't actually request a specific version. It's only being used here as a cache buster.
     url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=${version}";
-    hash = "sha256-hmnvrNKwj2Zx2e2YpVBoEQRJC4bJLjJBFIr6NipNKTg=";
+    hash = "sha256-LrGHVMq7dXMCNwHQQ+w3o+RoDcxSuwl5rJy1hi3fjTk=";
   };
 in
   stdenvNoCC.mkDerivation rec {
