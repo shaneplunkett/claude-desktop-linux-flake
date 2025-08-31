12880bf0f7a24a3fbf4e56df384312980e7512a3
Eval Exec <execvy@gmail.com>
2025-08-31 18:21:58 +0800

    Update claude-desktop sha256

--- pkgs/claude-desktop.nix
+++ pkgs/claude-desktop.nix
@@ -17,7 +17,7 @@
   srcExe = fetchurl {
     # NOTE: `?v=0.10.0` doesn't actually request a specific version. It's only being used here as a cache buster.
     url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=${version}";
-    hash = "sha256-LrGHVMq7dXMCNwHQQ+w3o+RoDcxSuwl5rJy1hi3fjTk=";
+    hash = "sha256-ISyVjtr9vGzCqq7oaDQd6h9kC7iumyo38z9VjuVCsu4=";
   };
 in
   stdenvNoCC.mkDerivation rec {
