346604b5967d053c966805d23bdea8ba08d1dad6
Shane Plunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-10-22 19:39:33 +1100

    Update hash for Claude-Setup-x64.exe

--- pkgs/claude-desktop.nix
+++ pkgs/claude-desktop.nix
@@ -17,7 +17,7 @@
   srcExe = fetchurl {
     # NOTE: `?v=0.10.0` doesn't actually request a specific version. It's only being used here as a cache buster.
     url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=${version}";
-    hash = "sha256-u7zw3Nv/iYd8k7D6/SQkbtsQ22V8xdq63+R4HMtapzE=";
+    hash = "sha256-d0hI6idjoKrlyMNNup36/zigwZbWpO0kkHxXaNhmJJ8=";
   };
 in
   stdenvNoCC.mkDerivation rec {
